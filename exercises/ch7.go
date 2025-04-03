101ea0861ea2e9dfec95bffa0bfc68337de0dbaa
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2025-04-03 19:19:52 +0000

    finishing methods in ch7

--- exercises/ch7.go
+++ exercises/ch7.go
@@ -3,6 +3,7 @@ package exercises
 import (
 	"fmt"
 	"strings"
+	"sort"
 )
 
 // EXERCISE 1: creating the structs
@@ -22,6 +23,11 @@ type League struct {
 	Wins  map[string]int
 }
 
+type kv struct {
+	key string
+	Value int
+}
+
 // EXERCISE 2: methods
 func (l *League) MatchResult(t1 Team, t2 Team, p1 int, p2 int, wins map[string]int) {
 	if strings.EqualFold(t1.Name, t2.Name) {
@@ -36,7 +42,18 @@ func (l *League) MatchResult(t1 Team, t2 Team, p1 int, p2 int, wins map[string]i
 }
 
 func (l League) Ranking() []string {
+	var arranged []kv
+	for k, v := range l.Wins{
+		arranged = append(arranged, kv{k, v})
+	}
+	sort.Slice(arranged, func(i, j int) bool {
+		return arranged[i].Value > arranged[j].Value
+	})
+	fmt.Println(arranged)
 	var standings []string
+	for _, team := range arranged{
+		standings = append(standings, team.key)
+	}
 	return standings
 }
 
@@ -61,5 +78,7 @@ func Ch7() {
 	theLeague.MatchResult(teamC, teamB, 10, 22, wins)
 	theLeague.MatchResult(teamC, teamA, 7, 21, wins)
 	theLeague.MatchResult(teamD, teamB, 11, 14, wins)
-	fmt.Println(theLeague)
+	theLeague.MatchResult(teamC, teamB, 3, 0, wins)
+	theLeague.MatchResult(teamC, teamD, 3, 0, wins)
+	fmt.Println(theLeague.Ranking())
 }
